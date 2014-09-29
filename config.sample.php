5ccd2f3e4ca4f926b313b914cd892241cd226f3d
Henno Täht <henno.taht@gmail.com>
2014-09-29 17:31:17 +0300

    Test

--- config.sample.php
+++ config.sample.php
@@ -4,3 +4,10 @@ define('DATABASE_HOSTNAME', '127.0.0.1');
 define('DATABASE_USERNAME', 'root');
 define('DATABASE_PASSWORD', '');
 define('DATABASE_DATABASE', PROJECT_NAME);
+die('Please make sure you have changed your development branch from master to your specific
+     branch and then remove this message from config.php<br>
+<ul>
+    <li></li>
+    <li></li>
+</ul>
+');
