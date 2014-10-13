2c19bac7847358b7246e59d23adf743395cea8a9
Maile.Rohiväli <maile.rohivali@gmail.com>
2014-10-13 14:58:58 +0300

    add gradwork files

---
+++ controllers/gradwork.php
@@ -0,0 +1,8 @@
+<?php
+
+class gradwork extends Controller
+{
+
+    function index()
+
+}
---
+++ views/gradwork/gradwork_index.php
@@ -0,0 +1,4 @@
+<div class="btn-group btn-group-lg">
+    <button type="button" class="btn btn-default">Eelkaitsmine</button>
+    <button type="button" class="btn btn-default">Lõputöö</button>
+</div>
