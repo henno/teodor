d480f8056fcc189cd3ae949df42a778844649fe4
Anneliloo <anneli.loo94@gmail.com>
2014-10-13 11:06:59 +0300

    6petaja ja 6pilase p2eviku ylevaate funktsionaalsus

---
+++ controllers/journal_student.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Anneli
+ * Date: 13.10.14
+ * Time: 10:29
+ */
+
+class journal_student {
+    function index(){
+    }
+} 
---
+++ views/journal_student/journal_student_index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Anneli
+ * Date: 13.10.14
+ * Time: 10:43
+ */
+
+namespace views\journal_student;
+
+
+class journal_student_index {
+
+} 
