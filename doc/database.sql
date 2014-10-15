75520a1a5f671b7d9eb94dca1eee3723c68a7e4d
Maile.Rohiväli <maile.rohivali@gmail.com>
2014-10-15 12:44:44 +0300

    add db dump

--- doc/database.sql
+++ doc/database.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.12
+-- version 4.1.14
 -- http://www.phpmyadmin.net
 --
--- Host: localhost:3306
--- Loomise aeg: Okt 08, 2014 kell 04:40 PM
--- Serveri versioon: 5.5.34
--- PHP versioon: 5.5.10
+-- Host: 127.0.0.1
+-- Loomise aeg: Okt 15, 2014 kell 11:44 EL
+-- Serveri versioon: 5.6.17
+-- PHP versioon: 5.5.12
 
 SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -22,18 +22,18 @@ SET time_zone = "+00:00";
 --
 
 DROP TABLE IF EXISTS `group`;
-CREATE TABLE `group` (
-  `group_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Autocreated',
-  `group_name` varchar(50) NOT NULL COMMENT 'Autocreated',
+CREATE TABLE IF NOT EXISTS `group` (
+  `group_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'autocreated',
+  `group_name` varchar(50) COLLATE utf8mb4_bin NOT NULL COMMENT 'autocreated',
   PRIMARY KEY (`group_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin AUTO_INCREMENT=2 ;
 
 --
 -- Andmete tõmmistamine tabelile `group`
 --
 
 INSERT INTO `group` (`group_id`, `group_name`) VALUES
-  (1, 'VS13');
+(1, 'VS_13');
 
 -- --------------------------------------------------------
 
@@ -42,18 +42,18 @@ INSERT INTO `group` (`group_id`, `group_name`) VALUES
 --
 
 DROP TABLE IF EXISTS `person`;
-CREATE TABLE `person` (
+CREATE TABLE IF NOT EXISTS `person` (
   `person_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `person_name` varchar(70) NOT NULL,
+  `person_name` varchar(70) COLLATE utf8mb4_bin NOT NULL,
   PRIMARY KEY (`person_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin AUTO_INCREMENT=2 ;
 
 --
 -- Andmete tõmmistamine tabelile `person`
 --
 
 INSERT INTO `person` (`person_id`, `person_name`) VALUES
-  (1, 'Maile Rohiväli');
+(1, 'Maile Rohiväli');
 
 -- --------------------------------------------------------
 
@@ -62,19 +62,65 @@ INSERT INTO `person` (`person_id`, `person_name`) VALUES
 --
 
 DROP TABLE IF EXISTS `student`;
-CREATE TABLE `student` (
+CREATE TABLE IF NOT EXISTS `student` (
   `person_id` int(10) unsigned NOT NULL,
   `group_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`person_id`),
-  KEY `group_id` (`group_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  PRIMARY KEY (`person_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Andmete tõmmistamine tabelile `student`
 --
 
 INSERT INTO `student` (`person_id`, `group_id`) VALUES
-  (1, 1);
+(1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `thesis`
+--
+
+DROP TABLE IF EXISTS `thesis`;
+CREATE TABLE IF NOT EXISTS `thesis` (
+  `thesis_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `thesis_title` varchar(255) NOT NULL,
+  `person_id_author` int(10) unsigned NOT NULL,
+  `person_id_instructor` int(10) unsigned DEFAULT NULL,
+  `thesis_instructor_confirmed_at` timestamp NULL DEFAULT NULL,
+  `thesis_title_confirmed_at` timestamp NULL DEFAULT NULL,
+  `thesis_defended_at` timestamp NULL DEFAULT NULL,
+  `thesis_file_id_draft` int(10) unsigned DEFAULT NULL,
+  `thesis_file_id_final` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`thesis_id`),
+  KEY `person_id_author` (`person_id_author`,`person_id_instructor`),
+  KEY `thesis_file_id_draft` (`thesis_file_id_draft`),
+  KEY `thesis_file_id_final` (`thesis_file_id_final`),
+  KEY `person_id_instructor` (`person_id_instructor`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Andmete tõmmistamine tabelile `thesis`
+--
+
+INSERT INTO `thesis` (`thesis_id`, `thesis_title`, `person_id_author`, `person_id_instructor`, `thesis_instructor_confirmed_at`, `thesis_title_confirmed_at`, `thesis_defended_at`, `thesis_file_id_draft`, `thesis_file_id_final`) VALUES
+(1, 'Mingi lõputöö', 1, NULL, NULL, NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `thesis_file`
+--
+
+DROP TABLE IF EXISTS `thesis_file`;
+CREATE TABLE IF NOT EXISTS `thesis_file` (
+  `thesis_file_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `thesis_id` int(10) unsigned NOT NULL,
+  `thesis_file_name` varchar(255) NOT NULL,
+  `thesis_file_content` longblob,
+  `thesis_file_uploaded_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`thesis_file_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -83,33 +129,32 @@ INSERT INTO `student` (`person_id`, `group_id`) VALUES
 --
 
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
+CREATE TABLE IF NOT EXISTS `user` (
   `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(25) NOT NULL,
-  `is_admin` tinyint(4) NOT NULL DEFAULT '0',
-  `password` varchar(255) NOT NULL,
-  `active` tinyint(3) unsigned NOT NULL DEFAULT '0',
-  `email` varchar(255) NOT NULL,
-  `deleted` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `UNIQUE` (`username`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+  `username` varchar(100) NOT NULL,
+  `is_admin` int(10) unsigned NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `active` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `deleted` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
 -- Andmete tõmmistamine tabelile `user`
 --
 
 INSERT INTO `user` (`user_id`, `username`, `is_admin`, `password`, `active`, `email`, `deleted`) VALUES
-  (1, 'demo', 0, 'demo', 1, '', 0);
+(1, 'demo', 1, 'demo', 1, '', 0);
 
 --
 -- Tõmmistatud tabelite piirangud
 --
 
 --
--- Piirangud tabelile `student`
+-- Piirangud tabelile `thesis`
 --
-ALTER TABLE `student`
-ADD CONSTRAINT `student_ibfk_2` FOREIGN KEY (`group_id`) REFERENCES `group` (`group_id`),
-ADD CONSTRAINT `student_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `person` (`person_id`);
+ALTER TABLE `thesis`
+  ADD CONSTRAINT `thesis_ibfk_2` FOREIGN KEY (`person_id_instructor`) REFERENCES `person` (`person_id`),
+  ADD CONSTRAINT `thesis_ibfk_1` FOREIGN KEY (`person_id_author`) REFERENCES `person` (`person_id`);
 SET FOREIGN_KEY_CHECKS=1;
