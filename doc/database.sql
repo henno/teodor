37cf049e8908bfdb2725a38d1def6f13074c8e91
Silja <silja.hagu@gmail.com>
2014-10-29 14:55:30 +0200

    Database dump

--- doc/database.sql
+++ doc/database.sql
@@ -3,41 +3,46 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 20, 2014 at 01:42 PM
--- Server version: 5.6.20
--- PHP Version: 5.5.15
+-- Loomise aeg: Okt 29, 2014 kell 01:54 PL
+-- Serveri versioon: 5.6.20
+-- PHP versioon: 5.5.15
 
 SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Database: `teodor`
+-- Andmebaas: `teodor`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `group`
+-- Tabeli struktuur tabelile `person`
 --
 
-DROP TABLE IF EXISTS `group`;
-CREATE TABLE IF NOT EXISTS `group` (
-`group_id` int(10) unsigned NOT NULL,
-  `group_name` varchar(25) CHARACTER SET utf8 NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+DROP TABLE IF EXISTS `person`;
+CREATE TABLE IF NOT EXISTS `person` (
+  `person_id` int(10) unsigned NOT NULL,
+  `person_name` varchar(70) NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `is_admin` tinyint(4) unsigned NOT NULL,
+  `deleted` tinyint(1) unsigned NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=4 ;
 
 --
--- Dumping data for table `group`
+-- Andmete tõmmistamine tabelile `person`
 --
 
-INSERT INTO `group` (`group_id`, `group_name`) VALUES
-(1, 'gr-1');
+INSERT INTO `person` (`person_id`, `person_name`, `password`, `is_admin`, `deleted`) VALUES
+  (1, 'Sammal Habe', 'sammal', 1, 0),
+  (2, 'King Pool', 'king', 1, 0),
+  (3, 'Muh V', 'muh', 0, 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `student`
+-- Tabeli struktuur tabelile `student`
 --
 
 DROP TABLE IF EXISTS `student`;
@@ -47,37 +52,37 @@ CREATE TABLE IF NOT EXISTS `student` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `student`
+-- Andmete tõmmistamine tabelile `student`
 --
 
 INSERT INTO `student` (`person_id`, `group_id`) VALUES
-(2, 1),
-(3, 1);
+  (2, 1),
+  (3, 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `subject`
+-- Tabeli struktuur tabelile `subject`
 --
 
 DROP TABLE IF EXISTS `subject`;
 CREATE TABLE IF NOT EXISTS `subject` (
-`subject_id` int(10) unsigned NOT NULL,
+  `subject_id` int(10) unsigned NOT NULL,
   `subject_name` varchar(70) CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
--- Dumping data for table `subject`
+-- Andmete tõmmistamine tabelile `subject`
 --
 
 INSERT INTO `subject` (`subject_id`, `subject_name`) VALUES
-(1, 'Metsateadused'),
-(2, 'Ornitoloogia');
+  (1, 'Metsateadused'),
+  (2, 'Ornitoloogia');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `teacher`
+-- Tabeli struktuur tabelile `teacher`
 --
 
 DROP TABLE IF EXISTS `teacher`;
@@ -86,16 +91,16 @@ CREATE TABLE IF NOT EXISTS `teacher` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `teacher`
+-- Andmete tõmmistamine tabelile `teacher`
 --
 
 INSERT INTO `teacher` (`person_id`) VALUES
-(1);
+  (1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `teacher_subjects`
+-- Tabeli struktuur tabelile `teacher_subjects`
 --
 
 DROP TABLE IF EXISTS `teacher_subjects`;
@@ -105,43 +110,43 @@ CREATE TABLE IF NOT EXISTS `teacher_subjects` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `teacher_subjects`
+-- Andmete tõmmistamine tabelile `teacher_subjects`
 --
 
 INSERT INTO `teacher_subjects` (`person_id`, `subject_id`) VALUES
-(1, 1),
-(3, 1),
-(1, 2);
+  (1, 1),
+  (3, 1),
+  (1, 2);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `test`
+-- Tabeli struktuur tabelile `test`
 --
 
 DROP TABLE IF EXISTS `test`;
 CREATE TABLE IF NOT EXISTS `test` (
-`test_id` int(11) unsigned NOT NULL,
+  `test_id` int(10) unsigned NOT NULL,
   `test_name` varchar(255) CHARACTER SET utf8 NOT NULL,
   `test_max_allowed_time` timestamp NULL DEFAULT NULL,
-  `test_max_allowed_attempts` int(11) unsigned DEFAULT NULL,
-  `test_min_score` int(11) DEFAULT NULL,
-  `person_id_author` int(11) unsigned NOT NULL,
-  `subject_id` int(11) unsigned DEFAULT NULL,
-  `lecture_id` int(11) unsigned DEFAULT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `test_max_allowed_attempts` int(10) unsigned DEFAULT NULL,
+  `test_min_score` int(10) DEFAULT NULL,
+  `person_id_author` int(10) unsigned NOT NULL,
+  `subject_id` int(10) unsigned DEFAULT NULL,
+  `lecture_id` int(10) unsigned DEFAULT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
--- Dumping data for table `test`
+-- Andmete tõmmistamine tabelile `test`
 --
 
 INSERT INTO `test` (`test_id`, `test_name`, `test_max_allowed_time`, `test_max_allowed_attempts`, `test_min_score`, `person_id_author`, `subject_id`, `lecture_id`) VALUES
-(1, 'Metsateaduste töö', '2014-10-12 21:15:00', 3, NULL, 1, 1, NULL);
+  (1, 'Metsateaduste töö', '2014-10-12 21:15:00', 3, NULL, 1, 1, NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `test_participants`
+-- Tabeli struktuur tabelile `test_participants`
 --
 
 DROP TABLE IF EXISTS `test_participants`;
@@ -153,21 +158,21 @@ CREATE TABLE IF NOT EXISTS `test_participants` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `test_participants`
+-- Andmete tõmmistamine tabelile `test_participants`
 --
 
 INSERT INTO `test_participants` (`person_id`, `test_id`, `test_started_at`, `test_ended_at`) VALUES
-(2, 1, '2014-10-13 13:20:38', '2014-10-13 13:20:38');
+  (2, 1, '2014-10-13 13:20:38', '2014-10-13 13:20:38');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `test_question`
+-- Tabeli struktuur tabelile `test_question`
 --
 
 DROP TABLE IF EXISTS `test_question`;
 CREATE TABLE IF NOT EXISTS `test_question` (
-`test_question_id` int(10) unsigned NOT NULL,
+  `test_question_id` int(10) unsigned NOT NULL,
   `test_question_text` text CHARACTER SET utf8 NOT NULL,
   `test_question_score` float NOT NULL,
   `test_id` int(10) unsigned NOT NULL,
@@ -175,22 +180,22 @@ CREATE TABLE IF NOT EXISTS `test_question` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
--- Dumping data for table `test_question`
+-- Andmete tõmmistamine tabelile `test_question`
 --
 
 INSERT INTO `test_question` (`test_question_id`, `test_question_text`, `test_question_score`, `test_id`, `test_question_type_id`) VALUES
-(1, 'Kas mets mühiseb?', 2, 1, 1),
-(2, 'Mitu puud moodustavad metsa?', 1, 1, 2);
+  (1, 'Kas mets mühiseb?', 2, 1, 1),
+  (2, 'Mitu puud moodustavad metsa?', 1, 1, 2);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `test_question_answer`
+-- Tabeli struktuur tabelile `test_question_answer`
 --
 
 DROP TABLE IF EXISTS `test_question_answer`;
 CREATE TABLE IF NOT EXISTS `test_question_answer` (
-`test_question_answer_id` int(10) unsigned NOT NULL,
+  `test_question_answer_id` int(10) unsigned NOT NULL,
   `test_question_answer_text` text CHARACTER SET utf8 NOT NULL,
   `test_question_correct` tinyint(3) unsigned NOT NULL,
   `test_question_type_id` int(10) unsigned NOT NULL,
@@ -198,290 +203,221 @@ CREATE TABLE IF NOT EXISTS `test_question_answer` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
--- Dumping data for table `test_question_answer`
+-- Andmete tõmmistamine tabelile `test_question_answer`
 --
 
 INSERT INTO `test_question_answer` (`test_question_answer_id`, `test_question_answer_text`, `test_question_correct`, `test_question_type_id`, `test_question_id`) VALUES
-(1, 'jah', 1, 1, 1),
-(2, 'ei', 0, 1, 1),
-(3, 'üks', 0, 2, 2),
-(4, 'kaks', 0, 2, 2),
-(5, 'palju', 1, 2, 2);
+  (1, 'jah', 1, 1, 1),
+  (2, 'ei', 0, 1, 1),
+  (3, 'üks', 0, 2, 2),
+  (4, 'kaks', 0, 2, 2),
+  (5, 'palju', 1, 2, 2);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `test_question_type`
+-- Tabeli struktuur tabelile `test_question_type`
 --
 
 DROP TABLE IF EXISTS `test_question_type`;
 CREATE TABLE IF NOT EXISTS `test_question_type` (
-`test_question_type_id` int(10) unsigned NOT NULL,
+  `test_question_type_id` int(10) unsigned NOT NULL,
   `test_question_type_name` varchar(50) CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
--- Dumping data for table `test_question_type`
+-- Andmete tõmmistamine tabelile `test_question_type`
 --
 
 INSERT INTO `test_question_type` (`test_question_type_id`, `test_question_type_name`) VALUES
-(1, 'true/false'),
-(2, 'multiple choice'),
-(3, 'multiple response'),
-(4, 'fill in the blank');
+  (1, 'true/false'),
+  (2, 'multiple choice'),
+  (3, 'multiple response'),
+  (4, 'fill in the blank');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `thesis`
---
-
-DROP TABLE IF EXISTS `thesis`;
-CREATE TABLE IF NOT EXISTS `thesis` (
-`thesis_id` int(10) unsigned NOT NULL,
-  `thesis_title` varchar(255) NOT NULL,
-  `person_id_author` int(10) unsigned NOT NULL,
-  `person_id_instructor` int(10) unsigned DEFAULT NULL,
-  `thesis_instructor_confirmed_at` timestamp NULL DEFAULT NULL,
-  `thesis_title_confirmed_at` timestamp NULL DEFAULT NULL,
-  `thesis_defended_at` timestamp NULL DEFAULT NULL,
-  `thesis_file_id_draft` int(10) unsigned DEFAULT NULL,
-  `thesis_file_id_final` int(10) unsigned DEFAULT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `thesis`
---
-
-INSERT INTO `thesis` (`thesis_id`, `thesis_title`, `person_id_author`, `person_id_instructor`, `thesis_instructor_confirmed_at`, `thesis_title_confirmed_at`, `thesis_defended_at`, `thesis_file_id_draft`, `thesis_file_id_final`) VALUES
-(1, 'Mingi lõputöö', 1, NULL, NULL, NULL, NULL, NULL, NULL);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `thesis_file`
---
-
-DROP TABLE IF EXISTS `thesis_file`;
-CREATE TABLE IF NOT EXISTS `thesis_file` (
-`thesis_file_id` int(10) unsigned NOT NULL,
-  `thesis_id` int(10) unsigned NOT NULL,
-  `thesis_file_name` varchar(255) NOT NULL,
-  `thesis_file_content` longblob,
-  `thesis_file_uploaded_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
+-- Tabeli struktuur tabelile `user`
 --
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
-`user_id` int(10) unsigned NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
   `username` varchar(25) NOT NULL,
   `is_admin` tinyint(4) NOT NULL DEFAULT '0',
   `password` varchar(255) NOT NULL,
   `active` tinyint(3) unsigned NOT NULL DEFAULT '0',
   `email` varchar(255) NOT NULL,
   `deleted` tinyint(1) unsigned NOT NULL DEFAULT '0'
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
--- Dumping data for table `user`
+-- Andmete tõmmistamine tabelile `user`
 --
 
 INSERT INTO `user` (`user_id`, `username`, `is_admin`, `password`, `active`, `email`, `deleted`) VALUES
-(1, 'demo', 0, 'demo', 1, '', 0);
+  (1, 'demo', 0, 'demo', 1, '', 0),
+  (2, 'asd', 1, 'asdasd', 0, 'asd@asd.asd', 0);
 
 --
--- Indexes for dumped tables
+-- Indeksid tõmmistatud tabelitele
 --
 
 --
--- Indexes for table `group`
+-- Indeksid tabelile `person`
 --
-ALTER TABLE `group`
- ADD PRIMARY KEY (`group_id`);
+ALTER TABLE `person`
+ADD PRIMARY KEY (`person_id`);
 
 --
--- Indexes for table `student`
+-- Indeksid tabelile `student`
 --
 ALTER TABLE `student`
- ADD PRIMARY KEY (`person_id`,`group_id`), ADD KEY `group_id` (`group_id`);
+ADD PRIMARY KEY (`person_id`,`group_id`), ADD KEY `group_id` (`group_id`);
 
 --
--- Indexes for table `subject`
+-- Indeksid tabelile `subject`
 --
 ALTER TABLE `subject`
- ADD PRIMARY KEY (`subject_id`);
+ADD PRIMARY KEY (`subject_id`);
 
 --
--- Indexes for table `teacher`
+-- Indeksid tabelile `teacher`
 --
 ALTER TABLE `teacher`
- ADD PRIMARY KEY (`person_id`);
+ADD PRIMARY KEY (`person_id`);
 
 --
--- Indexes for table `teacher_subjects`
+-- Indeksid tabelile `teacher_subjects`
 --
 ALTER TABLE `teacher_subjects`
- ADD PRIMARY KEY (`person_id`,`subject_id`), ADD KEY `subject_id` (`subject_id`);
+ADD PRIMARY KEY (`person_id`,`subject_id`), ADD KEY `subject_id` (`subject_id`);
 
 --
--- Indexes for table `test`
+-- Indeksid tabelile `test`
 --
 ALTER TABLE `test`
- ADD PRIMARY KEY (`test_id`), ADD KEY `person_id_author` (`person_id_author`), ADD KEY `subject_id` (`subject_id`);
+ADD PRIMARY KEY (`test_id`), ADD KEY `person_id_author` (`person_id_author`), ADD KEY `subject_id` (`subject_id`);
 
 --
--- Indexes for table `test_participants`
+-- Indeksid tabelile `test_participants`
 --
 ALTER TABLE `test_participants`
- ADD PRIMARY KEY (`person_id`,`test_id`), ADD KEY `test_id` (`test_id`);
+ADD PRIMARY KEY (`person_id`,`test_id`), ADD KEY `test_id` (`test_id`);
 
 --
--- Indexes for table `test_question`
+-- Indeksid tabelile `test_question`
 --
 ALTER TABLE `test_question`
- ADD PRIMARY KEY (`test_question_id`), ADD KEY `test_id` (`test_id`), ADD KEY `test_question_type_id` (`test_question_type_id`);
+ADD PRIMARY KEY (`test_question_id`), ADD KEY `test_id` (`test_id`), ADD KEY `test_question_type_id` (`test_question_type_id`);
 
 --
--- Indexes for table `test_question_answer`
+-- Indeksid tabelile `test_question_answer`
 --
 ALTER TABLE `test_question_answer`
- ADD PRIMARY KEY (`test_question_answer_id`), ADD KEY `test_question_type_id` (`test_question_type_id`), ADD KEY `test_question_id` (`test_question_id`);
+ADD PRIMARY KEY (`test_question_answer_id`), ADD KEY `test_question_type_id` (`test_question_type_id`), ADD KEY `test_question_id` (`test_question_id`);
 
 --
--- Indexes for table `test_question_type`
+-- Indeksid tabelile `test_question_type`
 --
 ALTER TABLE `test_question_type`
- ADD PRIMARY KEY (`test_question_type_id`);
+ADD PRIMARY KEY (`test_question_type_id`);
 
 --
--- Indexes for table `thesis`
---
-ALTER TABLE `thesis`
- ADD PRIMARY KEY (`thesis_id`), ADD KEY `person_id_author` (`person_id_author`,`person_id_instructor`), ADD KEY `thesis_file_id_draft` (`thesis_file_id_draft`), ADD KEY `thesis_file_id_final` (`thesis_file_id_final`), ADD KEY `person_id_instructor` (`person_id_instructor`);
-
---
--- Indexes for table `thesis_file`
---
-ALTER TABLE `thesis_file`
- ADD PRIMARY KEY (`thesis_file_id`);
-
---
--- Indexes for table `user`
+-- Indeksid tabelile `user`
 --
 ALTER TABLE `user`
- ADD PRIMARY KEY (`user_id`), ADD UNIQUE KEY `UNIQUE` (`username`);
+ADD PRIMARY KEY (`user_id`), ADD UNIQUE KEY `UNIQUE` (`username`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT tõmmistatud tabelitele
 --
 
 --
--- AUTO_INCREMENT for table `group`
+-- AUTO_INCREMENT tabelile `person`
 --
-ALTER TABLE `group`
-MODIFY `group_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+ALTER TABLE `person`
+MODIFY `person_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
--- AUTO_INCREMENT for table `subject`
+-- AUTO_INCREMENT tabelile `subject`
 --
 ALTER TABLE `subject`
 MODIFY `subject_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
--- AUTO_INCREMENT for table `test`
+-- AUTO_INCREMENT tabelile `test`
 --
 ALTER TABLE `test`
-MODIFY `test_id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `test_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
--- AUTO_INCREMENT for table `test_question`
+-- AUTO_INCREMENT tabelile `test_question`
 --
 ALTER TABLE `test_question`
 MODIFY `test_question_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
--- AUTO_INCREMENT for table `test_question_answer`
+-- AUTO_INCREMENT tabelile `test_question_answer`
 --
 ALTER TABLE `test_question_answer`
 MODIFY `test_question_answer_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
--- AUTO_INCREMENT for table `test_question_type`
+-- AUTO_INCREMENT tabelile `test_question_type`
 --
 ALTER TABLE `test_question_type`
 MODIFY `test_question_type_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
--- AUTO_INCREMENT for table `thesis`
---
-ALTER TABLE `thesis`
-MODIFY `thesis_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `thesis_file`
---
-ALTER TABLE `thesis_file`
-MODIFY `thesis_file_id` int(10) unsigned NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT tabelile `user`
 --
 ALTER TABLE `user`
-MODIFY `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
--- Constraints for dumped tables
+-- Tõmmistatud tabelite piirangud
 --
 
 --
--- Constraints for table `student`
+-- Piirangud tabelile `student`
 --
 ALTER TABLE `student`
 ADD CONSTRAINT `student_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `person` (`person_id`),
 ADD CONSTRAINT `student_ibfk_2` FOREIGN KEY (`group_id`) REFERENCES `group` (`group_id`);
 
 --
--- Constraints for table `teacher`
+-- Piirangud tabelile `teacher`
 --
 ALTER TABLE `teacher`
 ADD CONSTRAINT `teacher_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `person` (`person_id`);
 
 --
--- Constraints for table `teacher_subjects`
+-- Piirangud tabelile `teacher_subjects`
 --
 ALTER TABLE `teacher_subjects`
 ADD CONSTRAINT `teacher_subjects_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `person` (`person_id`),
 ADD CONSTRAINT `teacher_subjects_ibfk_2` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`subject_id`);
 
 --
--- Constraints for table `test`
+-- Piirangud tabelile `test`
 --
 ALTER TABLE `test`
 ADD CONSTRAINT `test_ibfk_1` FOREIGN KEY (`person_id_author`) REFERENCES `person` (`person_id`),
 ADD CONSTRAINT `test_ibfk_2` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`subject_id`);
 
 --
--- Constraints for table `test_participants`
+-- Piirangud tabelile `test_participants`
 --
 ALTER TABLE `test_participants`
 ADD CONSTRAINT `test_participants_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `person` (`person_id`),
 ADD CONSTRAINT `test_participants_ibfk_2` FOREIGN KEY (`test_id`) REFERENCES `test` (`test_id`);
 
 --
--- Constraints for table `test_question`
+-- Piirangud tabelile `test_question`
 --
 ALTER TABLE `test_question`
 ADD CONSTRAINT `test_question_ibfk_1` FOREIGN KEY (`test_id`) REFERENCES `test` (`test_id`),
 ADD CONSTRAINT `test_question_ibfk_2` FOREIGN KEY (`test_question_type_id`) REFERENCES `test_question_type` (`test_question_type_id`);
 
 --
--- Constraints for table `test_question_answer`
+-- Piirangud tabelile `test_question_answer`
 --
 ALTER TABLE `test_question_answer`
 ADD CONSTRAINT `test_question_answer_ibfk_1` FOREIGN KEY (`test_question_type_id`) REFERENCES `test_question_type` (`test_question_type_id`),
 ADD CONSTRAINT `test_question_answer_ibfk_2` FOREIGN KEY (`test_question_id`) REFERENCES `test_question` (`test_question_id`);
-
---
--- Constraints for table `thesis`
---
-ALTER TABLE `thesis`
-ADD CONSTRAINT `thesis_ibfk_1` FOREIGN KEY (`person_id_author`) REFERENCES `person` (`person_id`),
-ADD CONSTRAINT `thesis_ibfk_2` FOREIGN KEY (`person_id_instructor`) REFERENCES `person` (`person_id`);
 SET FOREIGN_KEY_CHECKS=1;
