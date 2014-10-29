5a6b7edb7ccd44d5dc004c61ae47eee8fdfec05b
Maile Rohiväli <maile.rohivali@gmail.com>
2014-10-29 13:23:53 +0200

    Move upload buttons to thesises front page

--- views/thesises/thesises_index.php
+++ views/thesises/thesises_index.php
@@ -1,4 +1,35 @@
 <h3><?__('Lõputööd')?></h3>
+<div class="row">
+    <div class="col-md-6">
+        <span class="lead">Laadi üles:</span>
+
+
+        <div class="pull-right">
+
+            <div class="btn-group btn-group-lg">
+                <button type="button" class="btn btn-default" id="thesis-draft">Eelkaitsmine </button>
+                <button type="button" class="btn btn-default">Lõputöö</button>
+                <button type="button" class="btn btn-default">Lisamaterjalid</button>
+            </div>
+        </div>
+        <form id="uploadForm" method="post" enctype="multipart/form-data">
+            <?php var_dump($_FILES) ?>
+            <input type="file" name="input_upload" id="input_upload" class="file-upload"/>
+            <input type="text" id="file-name" class="text-upload" readonly/>
+        </form>
+        <script>
+            $('#thesis-draft').click(function (event) {
+                $('#input_upload').click();
+            });
+
+            //capture selected filename
+            $('#input_upload').change(function (click) {
+                //  $('#file-name').val(this.value);
+                $('form#uploadForm').submit();
+            });
+        </script>
+    </div>
+</div>
 <? if (empty($thesises)): ?>
     <div class="alert alert-info"><?__('Hetkel lõputööde andmebaas on tühi.') ?></div>
     <? else: ?>
