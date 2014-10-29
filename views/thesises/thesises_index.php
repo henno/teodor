c675c1f87228c2480a330318aa38f985d57805fe
Maile Rohiväli <maile.rohivali@gmail.com>
2014-10-29 13:25:54 +0200

    Move thesis instructor and thesis name to thesises front page

--- views/thesises/thesises_index.php
+++ views/thesises/thesises_index.php
@@ -30,6 +30,28 @@
         </script>
     </div>
 </div>
+<div class="row">
+    <div class="col-lg-6">
+        <div class="input-group">
+      <span class="input-group-btn">
+        <button class="btn btn-default" type="button">Kinnita</button>
+      </span>
+            <input type="text" class="form-control" placeholder="Juhendaja nimi">
+        </div>
+        <!-- /input-group -->
+    </div>
+    <!-- /.col-lg-6 -->
+    <div class="col-lg-6">
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="Lõputöö teema">
+      <span class="input-group-btn">
+        <button class="btn btn-default" type="button">Kinnita</button>
+      </span>
+        </div>
+        <!-- /input-group -->
+    </div>
+    <!-- /.col-lg-6 -->
+</div>
 <? if (empty($thesises)): ?>
     <div class="alert alert-info"><?__('Hetkel lõputööde andmebaas on tühi.') ?></div>
     <? else: ?>
--- views/thesises/thesises_view.php
+++ views/thesises/thesises_view.php
@@ -1,24 +1,3 @@
 
 <br/>
-<div class="row">
-    <div class="col-lg-6">
-        <div class="input-group">
-      <span class="input-group-btn">
-        <button class="btn btn-default" type="button">Kinnita</button>
-      </span>
-            <input type="text" class="form-control" placeholder="Juhendaja nimi">
-        </div>
-        <!-- /input-group -->
-    </div>
-    <!-- /.col-lg-6 -->
-    <div class="col-lg-6">
-        <div class="input-group">
-            <input type="text" class="form-control" placeholder="Lõputöö teema">
-      <span class="input-group-btn">
-        <button class="btn btn-default" type="button">Kinnita</button>
-      </span>
-        </div>
-        <!-- /input-group -->
-    </div>
-    <!-- /.col-lg-6 -->
-</div><!-- /.row -->
+<!-- /.row -->
