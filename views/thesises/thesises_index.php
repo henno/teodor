f3d4ea03f6426f4cdfaadb4b65a054100dc0cb43
Maile Rohiväli <maile.rohivali@gmail.com>
2014-11-03 11:07:43 +0200

    changed thesis_index html

--- views/thesises/thesises_index.php
+++ views/thesises/thesises_index.php
@@ -1,56 +1,68 @@
-<h2><? __('Lõputööd') ?></h2>
+<h2 xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html"><? __('Lõputööd') ?></h2>
 <h3>Dokumentatsioon</h3>
-
-<h3>Kinnita lõputöö juhendaja</h3>
-<div class="row">
-    <div class="col-lg-6">
-        <form method="post">
-            <div class="input-group">
-                <select name="data[person_id_instructor]" data-placeholder="Juhendaja nimi" class="chosen-select" tabindex="2">
-                    <option value=""></option>
-                    <? foreach ($instructors as $instructor): ?>
-                        <option value="<?= $instructor['person_id'] ?>"><?= $instructor['person_name'] ?></option>
-                    <? endforeach ?>
-                </select>
-                <p>Kui ei leidnud sobivat juhendajat kirjuta juhendaja nimi ja ettevõte alljärgnevasse kasti</p>
-                <div class="input-group instructor">
-                    <input type="text" class="form-control" placeholder="Juhendaja nimi, ettevõte">
-                </div>
-                <span class="input-group-btn">
-                    <button class="btn btn-primary" type="submit">Kinnita</button>
-                </span>
+<form role="form" class="form-horizontal" method="post" action="tests/<?= $test['test_id'] ?>">
+    <div class="thesis_title"></div>
+        <div class="row">
+            <h3 class="col-sm-5">Kinnita lõputöö teema</h3>
+            <div class="form-group col-sm-7">
+                <form method="post">
+                    <div class="input-group">
+                        <select name="data[thesis_name]" data-placeholder="Lõputöö pealkiri" class="chosen-select" tabindex="2">
+                            <option value=""></option>
+                            <? foreach ($instructors as $instructor): ?>
+                                <option value="<?= $instructor['person_id'] ?>"><?= $instructor['person_name'] ?></option>
+                            <? endforeach ?>
+                        </select>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="row">
+            <label class="col-sm-5 control-label" for="thesis_title">Lõputöö teema</label>
+            <div class="col-sm-7">
+                <input type="text" class="form-control" name="data[test_name]" id="test_name"
+                       placeholder="Siia kirjuta lõputöö teema" value="<?= $test['test_name'] ?>">
+            </div>
+        </div>
+    </div>
+    <div class="instructor"
+        <div class="row">
+            <h3 class="col-sm-5">Kinnita juhendaja nimi</h3>
+            <div class="form-group col-sm-7">
+                <form method="post">
+                    <div class="input-group">
+                        <select name="data[thesis_name]" data-placeholder="Juhendaja nimi" class="chosen-select" tabindex="2">
+                            <option value=""></option>
+                            <? foreach ($instructors as $instructor): ?>
+                                <option value="<?= $instructor['person_id'] ?>"><?= $instructor['person_name'] ?></option>
+                            <? endforeach ?>
+                        </select>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="row">
+            <label class="col-sm-5 control-label" for="instructor">Juhendaja nimi ja ettevõte</label>
+            <div class="col-sm-7">
+                <input type="text" class="form-control" name="data[person_id_instructor]" id="test_name"
+                       placeholder="Siia kirjuta juhendaja kui ei leidnud valikust sobivat" value="<?= $test['test_name'] ?>">
+            </div>
+        </div>
+    </div>
+    <!-- EDIT BUTTON -->
+    <? if ($auth->is_admin): ?>
+        <form >
+            <div class="pull-right">
+                <button class="btn btn-primary">
+                    Salvesta
+                </button>
             </div>
         </form>
-        <!-- /input-group -->
-    </div>
-    <!-- /.col-lg-6 -->
-</div>
-<h3>Kinnita lõputöö teema</h3>
-<div class="row">
-    <div class="col-lg-6">
-        <form method="post">
-            <div class="input-group">
-                <select name="data[thesis_name]" data-placeholder="Lõputöö pealkiri" class="chosen-select" tabindex="2">
-                    <option value=""></option>
-                    <? foreach ($instructors as $instructor): ?>
-                        <option value="<?= $instructor['person_id'] ?>"><?= $instructor['person_name'] ?></option>
-                    <? endforeach ?>
-                </select>
-                <p>Kui ei leidnud sobivat teemat, kirjuta teema alljärgnevasse kasti</p>
-                <div class="input-group thesis_name">
-                    <input type="text" class="form-control" placeholder="Lõputöö pealkiri">
-                </div>
-                <span class="input-group-btn">
-                    <button class="btn btn-primary" type="submit">Kinnita</button>
-                </span>
-            </div>
-        </form>
-        <!-- /input-group -->
-    </div>
-    <!-- /.col-lg-6 -->
-</div>
+    <? endif; ?>
+</form>
+<div class="row upload_files">
 <h3>Laadi sobivad failid</h3>
-<div class="row">
+
     <div class="col-md-6">
         <span class="lead">Laadi üles:</span>
         <div class="pull-right">
