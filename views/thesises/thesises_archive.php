9a2aa64acb0a275c57d43cc1623419998f0479ac
kcrolin <carolin.karing@khk.ee>
2014-11-18 23:43:56 +0000

    Created table for thesises

--- views/thesises/thesises_archive.php
+++ views/thesises/thesises_archive.php
@@ -1,13 +1,33 @@
 <? if (empty($thesises)): ?>
     <div class="alert alert-info"><?__('Hetkel lõputööde andmebaas on tühi.') ?></div>
 <? else: ?>
-    <ul class="list-group">
+
+    <div class="btn-group" role="group" aria-label="...">
+        <button type="button" class="btn btn-default">Väljapakutud ideed</button>
+        <button type="button" class="btn btn-default">Kinnitamisel</button>
+        <button type="button" class="btn btn-default">Teostamisel</button>
+        <button type="button" class="btn btn-default">Eelnevate aastate lõputööd</button>
+    </div>
+
+    <table class="table table-bordered">
+        <thead>
+        <tr>
+            <th>#</th>
+            <th>Teema</th>
+            <th>Tegevused</th>
+        </tr>
+        </thead>
+        <tbody>
         <? foreach ($thesises as $thesis): ?>
-            <li class="list-group-item">
-                <a href="thesises/view/<?= $thesis['thesis_id'] ?>/<?= $thesis['thesis_title'] ?>"><?= $thesis['thesis_title'] ?></a>
-            </li>
+            <tr>
+                <td><?= $thesis['thesis_id'] ?></td>
+                <td><a href="thesises/view/<?= $thesis['thesis_id'] ?>/<?= $thesis['thesis_title'] ?>"><?= $thesis['thesis_title'] ?></a>
+                </td>
+                <td>...</td>
+            </tr>
         <? endforeach ?>
-    </ul>
+        <tbody>
+    </table>
 
     <?php if ($auth->is_admin): ?>
 
