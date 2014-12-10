b02ca4bcd1794bf16f860962776830ec504ea6e5
Maile Rohiväli <maile.rohivali@gmail.com>
2014-12-10 12:13:32 +0200

    Add new subpages to thesises, change input in thesises_view

---
+++ views/thesises/thesises_edit.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Maile
+ * Date: 10.12.2014
+ * Time: 12:07
+ */ 
--- views/thesises/thesises_view.php
+++ views/thesises/thesises_view.php
@@ -2,16 +2,16 @@
 <form id="form" method="post">
     <table class="table table-bordered">
         <tr>
-            <td><input type="text" name="data[thesis_title]" value="<?= $thesis['thesis_title'] ?>"/></td>
-            <td><input type="text" name="data[thesis_title]" value="<?= $thesis['thesis_description'] ?>"/></td>
-            <td><input type="text" name="data[thesis_title]" value="<?= $thesis['thesis_client_info'] ?>"/></td>
-            <td><input type="text" name="data[thesis_title]" value="<?= $thesis['author_name'] ?>"/></td>
-            <td><input type="text" name="data[thesis_title]" value="<?= $thesis['instructor_name'] ?>"/></td>
-            <td><input type="text" name="data[thesis_title]" value="<?= $thesis['thesis_instructor_confirmed_at'] ?>"/></td>
-            <td><input type="text" name="data[thesis_title]" value="<?= $thesis['thesis_title_confirmed_at'] ?>"/></td>
-            <td><input type="text" name="data[thesis_title]" value="<?= $thesis['thesis_defended_at'] ?>"/></td>
-            <td><input type="text" name="data[thesis_title]" value="<?= $thesis['thesis_file_id_draft'] ?>"/></td>
-            <td><input type="text" name="data[thesis_title]" value="<?= $thesis['thesis_file_id_final'] ?>"/></td>
+            <td><input type="text" value="<?= $thesis['thesis_title'] ?>"/></td>
+            <td><input type="text" value="<?= $thesis['thesis_description'] ?>"/></td>
+            <td><input type="text" value="<?= $thesis['thesis_client_info'] ?>"/></td>
+            <td><input type="text" value="<?= $thesis['author_name'] ?>"/></td>
+            <td><input type="text" value="<?= $thesis['instructor_name'] ?>"/></td>
+            <td><input type="text" value="<?= $thesis['thesis_instructor_confirmed_at'] ?>"/></td>
+            <td><input type="text" value="<?= $thesis['thesis_title_confirmed_at'] ?>"/></td>
+            <td><input type="text" value="<?= $thesis['thesis_defended_at'] ?>"/></td>
+            <td><input type="text" value="<?= $thesis['thesis_file_id_draft'] ?>"/></td>
+            <td><input type="text" value="<?= $thesis['thesis_file_id_final'] ?>"/></td>
         </tr>
     </table>
 </form>
