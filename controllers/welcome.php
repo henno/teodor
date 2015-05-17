6f453b0cb2cce0c0fdb4c9e37afa073f0d2e2733
Henno Täht <henno.taht@gmail.com>
2015-05-17 18:24:00 +0300

    Remove var_dump of POST after login

--- controllers/welcome.php
+++ controllers/welcome.php
@@ -5,13 +5,4 @@ class welcome extends Controller{
 	function index(){
 	}
 
-    function index_ajax(){
-		echo "\$_POST:<br>";
-        var_dump($_POST);
-    }
-
-	function index_post(){
-		echo "\$_POST:<br>";
-		var_dump($_POST);
-	}
 }
