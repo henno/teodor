3f0521a0900680772ef8b018be3dfa2eeec12998
rasmusreimaa <rasmus.reimaa@khk.ee>
2016-03-21 14:03:15 +0200

    courses.php controller mysql fix

--- controllers/courses.php
+++ controllers/courses.php
@@ -19,11 +19,15 @@ class courses extends Controller
                   join periods USING (period_id)
                   join years USING (year_id)
                   join groups USING (group_id)
-                  natural join tasks 
+                  join course_tasks USING (course_id)
+                  join tasks USING (task_id)
                 WHERE courses.teacher_id = $my_id";
         $this->period_courses = get_all($sql);
         $this->periods = get_all("SELECT * FROM periods");
-        $this->taught_courses = get_all("SELECT * FROM courses NATURAL JOIN subjects NATURAL JOIN groups WHERE courses.teacher_id={$my_id}");
+        $this->taught_courses = get_all("SELECT * FROM courses 
+                                                  JOIN subjects USING (subject_id) 
+                                                  JOIN groups USING (group_id)
+                                                  WHERE courses.teacher_id={$my_id}");
         $this->courses_taken = get_all("SELECT * FROM group_persons gp
                                                  JOIN courses c ON c.group_id = gp.group_id
                                                  JOIN groups g ON g.group_id = c.group_id
@@ -38,7 +42,13 @@ class courses extends Controller
         return get_one("SELECT period_id FROM periods WHERE now() BETWEEN period_start AND period_end");
     }
     function view() {
-        $this->course = get_first("SELECT * FROM courses NATURAL JOIN persons NATURAL JOIN subjects NATURAL JOIN groups WHERE course_id = '{$this->params[0]}'");
+        $this->course = get_first("SELECT * FROM courses 
+                                          
+                                            JOIN subjects USING (subject_id)
+                                            JOIN groups USING (group_id)
+                                            JOIN group_persons USING (group_id)
+                                            JOIN persons USING (person_id)
+                                            WHERE course_id = '{$this->params[0]}'");
         $this->task = get_first("SELECT * FROM tasks");
     }
 }
